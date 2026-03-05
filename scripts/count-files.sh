1a10cd10f167da8e37d0b21180ef6636bec23e92
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-05 16:16:29 +0200

    docs: Add comments to count-files

--- scripts/count-files.sh
+++ scripts/count-files.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
+# If the arg count is not 0
 if [ $# -ne 0 ]; then
+  # Store the directory provided by the user
   dir=$1
 else
+  # Set to the working directory
   dir="."
 fi
 
+# Count all files and directories in the provided directory
 fcount=$(ls "$dir" -A1 | wc -l)
+
+# Get the resolved directory name from user input.
+# Works even if the user provides "../" or "./" as the
+# directory to count in.
 dispDir=$(basename "$(realpath $dir)")
 
+# Display result to the shell.
 echo "Count of files and directories in directory \"${dispDir}\": ${fcount}"
