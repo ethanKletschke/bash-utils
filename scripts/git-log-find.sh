897be58463e7e0242fdab014a77cc09ce243b3aa
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-12 11:52:15 +0200

    docs(scripts): Add comments to git-log-find

--- scripts/git-log-find.sh
+++ scripts/git-log-find.sh
@@ -7,15 +7,20 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
+# If the user provides an empty argument
 if [[ $1 == "" ]]; then
+  # Display an error message and exit
   echo "Commit type cannot be empty."
   exit 1
 fi
 
 # If the first argument provided isn't alphanumeric
 if [[ ! $1 =~ ^[a-zA-Z0-9]+$ ]]; then
+  # Display an error message and exit
   echo "Invalid Conventional Commit type. Must be alphanumeric."
   exit 1
 fi
 
+# Search for the specified commit type, including the optional scope and
+# "!" (breaking change) symbol
 git log --oneline | grep -iE "$1(\([a-zA-Z0-9]*\))?(!)?:"
