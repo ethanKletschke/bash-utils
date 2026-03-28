40542109f0567751ebb5716967f5856032342094
Ethan Kletschke <bbruhingson@gmail.com>
2026-03-28 05:53:20 +0200

    feat: Add support for a `-t` flag in `count-files`

--- scripts/count-files.sh
+++ scripts/count-files.sh
@@ -1,5 +1,35 @@
 #!/usr/bin/env bash
 
+while getopts "t" opt; do
+  case "$opt" in
+    t)
+      for file in *; do
+        txtCount=0
+        scriptCount=0
+        otherCount=0
+
+        case "$file" in
+          *.txt)
+            ((txtCount++))
+            ;;
+          *.sh)
+            ((scriptCount++))
+            ;;
+          *)
+            ((otherCount++))
+            ;;
+        esac
+      done
+
+      echo "Text Files: $txtCount"
+      echo "Scripts: $scriptCount"
+      echo "Other: $otherCount"
+
+      exit 0
+      ;;
+  esac
+done
+
 # If the arg count is not 0
 if [ $# -ne 0 ]; then
   # Store the directory provided by the user
